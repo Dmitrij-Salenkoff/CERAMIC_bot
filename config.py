48923d4cf2a112d5be62b1b5da338286463a266c
dsalenkoff <dmitrij.salenkoff@gmail.com>
2023-04-15 01:08:09 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = '5423698250:AAGPt8kbZnNXIofr8n7J22-3BC_9bU9xAiA'
-ADMINS_ID = [880801285]
-password = '8od1mDDE'
+TOKEN = ''
+ADMINS_ID = []
+password = ''
 
 DB_FILENAME = 'botuploads.db'
 
